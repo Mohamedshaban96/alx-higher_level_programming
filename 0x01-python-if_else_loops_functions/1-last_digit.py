c5adc25a42a64698b7509a76052e4445991610b1
Mohamedshaban96 <mohamedshaban1234@gmail.com>
2023-10-31 18:10:03 -0700

    hot fix 4 t1

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -3,7 +3,7 @@ import random
 number = random.randint(-10000, 100000)
 ladi = abs(number) % 10
 if number < 0:
-    ladi = ladi * -1
+    ladi = -ladi
 if ladi > 5:
     print(f"Last digit of {number:d} is {ladi:d} and is greater than 5")
 elif ladi == 0:
