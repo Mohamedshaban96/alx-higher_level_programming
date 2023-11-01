c86ec11261aa48aa744636a913ef71304a049c84
Mohamedshaban96 <mohamedshaban1234@gmail.com>
2023-10-31 18:26:53 -0700

    t1

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -5,8 +5,8 @@ ladi = abs(number) % 10
 if number < 0:
     ladi = -ladi
 if ladi > 5:
-    print(f"Last digit of {number:d} is {ladi:d} and is greater than 5")
+    print(f"Last digit of {number:d} is {ladi :d} and is greater than 5")
 elif ladi == 0:
-    print(f"Last digit of {number:d} is {ladi:d} and is 0")
+    print(f"Last digit of {number:d} is {ladi :d} and is 0")
 else:
-    print(f"Last digit of {number:d} is {ladi:d} and is less than 6 and not 0")
+    print(f"Last digit of {number:d} is {ladi :d} and is less than 6 and not 0")
