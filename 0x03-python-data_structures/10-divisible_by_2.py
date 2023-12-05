1949180dc2fb3ddb34bf15de8baea1793f7d13e9
Mohamedshaban96 <mohamedshaban1234@gmail.com>
2023-12-05 12:29:57 -0800

    t10

---
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def divisible_by_2(my_list=[]):
+    newl = []
+    for i in my_list:
+        if i % 2 == 0:
+            newl.append(True)
+        else:
+            newl.append(False)
+
+    return newl
