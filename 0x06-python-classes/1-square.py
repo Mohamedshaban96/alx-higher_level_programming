f62353ffacb710c0e77b828133075544da38dd82
Mohamedshaban96 <mohamedshaban1234@gmail.com>
2023-12-05 16:03:42 -0800

    t1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""creating a Square class"""
+
+
+class Square:
+    """a square class """
+    def __init__(self, size):
+        """setting inistant attributes"""
+        self.__size = size
