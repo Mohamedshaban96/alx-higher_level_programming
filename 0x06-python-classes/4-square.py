c7e10708cc6d2f3bef351e1118d31e43d3e26551
Mohamedshaban96 <mohamedshaban1234@gmail.com>
2024-01-06 21:12:39 +0000

    t4

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""creating a Square class"""
+
+
+class Square:
+	"""a square class """
+
+	def __init__(self, size=0):
+		"""setting inistant attributes"""
+		self.__size = size
+
+	def area(self):
+		"""function return the area of the square"""
+		return self.__size * self.__size
+
+	@property
+	def size(self):
+		"""method that return the size of the square"""
+		return self.__size
+
+	@size.setter
+		def size(self, value):
+		"""method to set up the size of the square"""
+		self.__size = value
+
+		if type(value) is not int:
+			raise TypeError("size must be an integer")
+
+		if value < 0:
+			raise ValueError("size must be >= 0")
