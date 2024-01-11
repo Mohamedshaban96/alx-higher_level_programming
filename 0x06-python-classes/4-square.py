c6b672785abae22e8bd2ab89bf8b0216a8f456e9
Mohamedshaban96 <mohamedshaban1234@gmail.com>
2024-01-10 17:20:08 -0800

    pycode stylling

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,30 +1,34 @@
 #!/usr/bin/python3
-	"""creating a Square class"""
+"""creating a Square class"""
 
 
 class Square:
-	"""a square class """
+    """a square class"""
 
-	def __init__(self, size=0):
-		"""setting inistant attributes"""
-		self.__size = size
+    def __init__(self, size=0):
+        """setting inistant attributes"""
 
-	def area(self):
-		"""function return the area of the square"""
-		return self.__size * self.__size
+    self.__size = size
 
-	@property
-	def size(self):
-		"""method that return the size of the square"""
-		return self.__size
+    def area(self):
+        """function return the area of the square"""
 
-	@size.setter
-		def size(self, value):
-			"""method to set up the size of the square"""
-		self.__size = value
+        return self.__size * self.__size
 
-		if type(value) is not int:
-			raise TypeError("size must be an integer")
+    @property
+    def size(self):
+        """method that return the size of the square"""
 
-		if value < 0:
-			raise ValueError("size must be >= 0")
+    return self.__size
+
+    @size.setter
+    def size(self, value):
+        """method to set up the size of the square"""
+
+        self.__size = value
+
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+
+        if value < 0:
+            raise ValueError("size must be >= 0")
