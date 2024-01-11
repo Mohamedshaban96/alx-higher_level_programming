0db693ef092c354c4104ed98a16b70518cba6a81
Mohamedshaban96 <mohamedshaban1234@gmail.com>
2024-01-10 17:26:47 -0800

    pycode stylling

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
-"""creating a Square class
-"""
+"""This module contains a class that defines a square.
+In the Square class we initialize each object by the
+__init__ method with a private instance variable called
+__size that takes the size variable's value passed as
+argument. Also checks if the size arg has a valid value.
+area method returns the area of the square."""
 
 
 class Square:
